fe119bae06e6de19cf275209888d536558a8a448
zhaolic39 <zhaolic39@gmail.com>
2014-06-24 23:46:00 +0800

    修改包名

---
+++ src/main/java/net/csf/annotation/ServiceBean.java
@@ -0,0 +1,17 @@
+package net.csf.annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+/**
+ * 用于定义同步命令的处理 
+ * @date 2011 Apr 29, 2011 5:25:01 PM
+ */
+@Documented
+@Target({ElementType.TYPE}) 
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface ServiceBean {
+}
